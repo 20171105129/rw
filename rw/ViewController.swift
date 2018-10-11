942ccd09d05c8e6e58ed250d8772dcee9b943fb0
20171105129 <3038914421@qq.com>
2018-10-11 10:02:20 +0800

    swift1
    完成最基本的

--- rw/ViewController.swift
+++ rw/ViewController.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var temp = 0
+    @IBOutlet var window: UITextField!
+    @IBAction func one(_ sender: Any) {
+        window.text = window.text!+"1"
+    }
+    @IBAction func two(_ sender: Any) {
+        window.text = window.text!+"2"
+    }
+    @IBAction func three(_ sender: Any) {
+        window.text = window.text!+"3"
+    }
+    @IBAction func four(_ sender: Any) {
+        window.text = window.text!+"4"
+    }
+    @IBAction func five(_ sender: Any) {
+        window.text = window.text!+"5"
+    }
+    @IBAction func six(_ sender: Any) {
+        window.text = window.text!+"6"
+    }
+    @IBAction func seven(_ sender: Any) {
+        window.text = window.text!+"7"
+    }
+    @IBAction func eight(_ sender: Any) {
+        window.text = window.text!+"8"
+    }
+    @IBAction func nine(_ sender: Any) {
+        window.text = window.text!+"9"
+    }
+    @IBAction func zeio(_ sender: Any) {
+        window.text = window.text!+"0"
+    }
+    @IBAction func and(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
